2322bf8506aae55fdb2fec486ff85fb9f99d7fc5
dutu <du.tu@hitachivantara.com>
2024-08-13 09:15:09 +0700

    update

--- components/grid.cards.tsx
+++ components/grid.cards.tsx
@@ -1,5 +1,6 @@
 'use client'
 import 'bootstrap/dist/css/bootstrap.min.css';
+import { ListGroup } from 'react-bootstrap';
 import Card from 'react-bootstrap/Card';
 import Col from 'react-bootstrap/Col';
 import Row from 'react-bootstrap/Row';
@@ -18,6 +19,15 @@ function GridCard() {
                                 lead-in to additional content. This content is a little bit
                                 longer.
                             </Card.Text>
+                        </Card.Body>
+                        <ListGroup className="list-group-flush">
+                            <ListGroup.Item>Cras justo odio</ListGroup.Item>
+                            <ListGroup.Item>Dapibus ac facilisis in</ListGroup.Item>
+                            <ListGroup.Item>Vestibulum at eros</ListGroup.Item>
+                        </ListGroup>
+                        <Card.Body>
+                            <Card.Link href="#">Card Link</Card.Link>
+                            <Card.Link href="#">Another Link</Card.Link>
                         </Card.Body>
                     </Card>
                 </Col>
