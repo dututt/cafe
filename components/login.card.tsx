7116504f7177afe2effaa231bd03bccd9c5dd8f5
dutu <du.tu@hitachivantara.com>
2024-08-13 17:10:49 +0700

    update

--- components/login.card.tsx
+++ components/login.card.tsx
@@ -9,7 +9,7 @@ function LoginCard() {
                 <Form.Label>Email address</Form.Label>
                 <Form.Control type="email" placeholder="Enter email" />
                 <Form.Text className="text-muted">
-                    We&apos;'ll never share your email with anyone else.
+                    We&apos;ll never share your email with anyone else.
                 </Form.Text>
             </Form.Group>
 
--- components/view.cards.tsx
+++ components/view.cards.tsx
@@ -10,7 +10,7 @@ function ViewCard() {
                     <Card.Title>Primary Card Title</Card.Title>
                     <Card.Text>
                         Some quick example text to build on the card title and make up the
-                        bulk of the card&apos;'s content.
+                        bulk of the card&apos;s content.
                     </Card.Text>
                 </Card.Body>
             </Card>
@@ -21,7 +21,7 @@ function ViewCard() {
                     <Card.Title>Secondary Card Title</Card.Title>
                     <Card.Text>
                         Some quick example text to build on the card title and make up the
-                        bulk of the card&apos;'s content.
+                        bulk of the card&apos;s content.
                     </Card.Text>
                 </Card.Body>
             </Card>
