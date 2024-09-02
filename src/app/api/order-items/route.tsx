ffb0c1af834aaef3f5b32dbfcad4119923af1e9d
dutu <du.tu@hitachivantara.com>
2024-09-02 19:51:12 +0700

    update

--- src/app/api/order-items/route.tsx
+++ src/app/api/order-items/route.tsx
@@ -1,4 +1,3 @@
-'use server'
 import { sql } from "@vercel/postgres";
 import { NextRequest, NextResponse } from "next/server";
 
@@ -9,6 +8,6 @@ export async function GET(res: NextRequest) {
         const result = await sql`SELECT i.title, i.image FROM order_items as oi INNER JOIN item as i ON oi.item_id = i.id WHERE oi.order_id = ${order_id}`;
         return NextResponse.json(result.rows, { status: 200 });
     } catch (error) {
-        return NextResponse.json({ error }, { status: 500 });
+        return NextResponse.json({ error: 'Database error' }, { status: 500 })
     }
 }
