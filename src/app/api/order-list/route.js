1bacd49e486d9d0cac977098a81932529a1ee25a
dutu <du.tu@hitachivantara.com>
2024-09-09 18:05:55 +0700

    update

--- src/app/api/order-list/route.js
+++ src/app/api/order-list/route.js
@@ -1,12 +1,13 @@
 'use server'
 import { sql } from "@vercel/postgres";
+import { headers } from "next/dist/client/components/headers";
 import { NextRequest, NextResponse } from "next/server";
 
 export async function GET() {
     try {
         console.log("order-list request")
         const result = await sql`SELECT o.id, o.table_num, o.price, o.created_at, COUNT(*) as count_items FROM orders as o INNER JOIN order_items as oi ON o.id = oi.order_id GROUP BY o.id, o.table_num`;
-        return NextResponse.json(result.rows, { status: 200 })
+        return NextResponse.json(result.rows, { status: 200 }, { headers: ['Cache-Control', 'no-store'] })
     } catch (error) {
         return NextResponse.json({ error }, { status: 500 })
     }
