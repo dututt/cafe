3d31cf335d9ce74f74fbe8d1fd158bf62098a247
dutu <du.tu@hitachivantara.com>
2024-09-10 15:17:09 +0700

    update

--- src/app/api/order-list/route.js
+++ src/app/api/order-list/route.js
@@ -1,6 +1,6 @@
 'use server'
 import { sql } from "@vercel/postgres";
-import { NextRequest, NextResponse } from "next/server";
+import { NextResponse } from "next/server";
 
 
 export async function GET() {
--- src/components/admin.tsx
+++ src/components/admin.tsx
@@ -1,6 +1,17 @@
 import { Button, ButtonGroup } from "react-bootstrap";
 import { useEffect, useState } from "react";
 
+export async function getStaticProps() {
+    const response = await fetch('/api/order-list');
+    const data = await response.json();
+    console.log(">>>>>>>>>>>>1111 fetchData: ", data)
+    return {
+        props: { data },
+        revalidate: 4, // Revalidate every 10 seconds
+    };
+}
+
+
 function Admin() {
     const [status, setStatus] = useState(false)
 
@@ -8,21 +19,22 @@ function Admin() {
         setStatus(true)
     }
 
-    console.log(">>>>>>>>>>>>0000 fetchData: ", status)
-    const fetchData = async () => {
-        const response = await fetch('/api/order-list', {
-            next: { revalidate: 10 } // Revalidate every 10 seconds
-        });
-        const data = await response.json();
-        console.log(">>>>>>>>>>>>1111 fetchData: ", data)
-        setStatus(false)
-        return data;
-    };
+    // console.log(">>>>>>>>>>>>0000 fetchData: ", status)
+    // const fetchData = async () => {
+    //     const response = await fetch('/api/order-list', {
+    //         next: { revalidate: 4 } // Revalidate every 10 seconds
+    //     });
+    //     const data = await response.json();
+    //     console.log(">>>>>>>>>>>>1111 fetchData: ", data)
+    //     setStatus(false)
+    //     return data;
+    // };
+
 
     useEffect(() => {
         console.log(">>>>>>>>>>>>22222 fetchData- status: ", status)
-        fetchData()
-    }, [status])
+        getStaticProps()
+    })
 
 
     return (
