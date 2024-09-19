590442ddfa1fbf51ac439aaf2c5e7486a3a57c82
dutu <du.tu@hitachivantara.com>
2024-09-19 11:28:53 +0700

    update

--- src/components/get.order.list.tsx
+++ src/components/get.order.list.tsx
@@ -1,19 +1,28 @@
+import { useEffect, useState } from "react";
+import { Button } from "react-bootstrap";
 
 export default function OrderItems() {
+    const [refresh, setRefresh] = useState(false)
 
-    const data = fetch("/api/order-list")
-        .then((res) => {
-            console.log("111Data rendering on server....")
-            return res.json();
-        })
+    useEffect(() => {
+        handleRefresh
+    }, [refresh])
 
-    if (!data) {
-        return <div>Orders loading...</div>
+    function handleRefresh() {
+        const data = fetch("/api/order-list")
+            .then((res) => {
+                console.log("111Data rendering on server....")
+                return res.json();
+            })
+
+        if (!data) {
+            return <div>Orders loading...</div>
+        }
     }
 
     return (
         <>
-
+            <Button variant="primary" onClick={() => handleRefresh()}>Refresh</Button>
         </>
     );
 }
