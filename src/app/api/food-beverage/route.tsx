6f8105106c40e9395f60c31b5bd423a9ebc750c1
dutu <du.tu@hitachivantara.com>
2024-09-22 12:24:17 +0700

    update

--- src/app/api/food-beverage/route.tsx
+++ src/app/api/food-beverage/route.tsx
@@ -2,8 +2,8 @@ import { NextResponse } from "next/server";
 
 export async function GET() {
     try {
-        // const response = await fetch('https://api-cafe-three.vercel.app/api/food-beverage');
-        const response = await fetch('http://localhost:3001/api/food-beverage');
+        const response = await fetch('https://api-cafe-three.vercel.app/api/food-beverage');
+        // const response = await fetch('http://localhost:3001/api/food-beverage');
         const data = await response.json();
 
         return NextResponse.json(data, { status: 200 })
