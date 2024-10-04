35b684cf495c13333653f3f9b20526d41b026448
dutu <du.tu@hitachivantara.com>
2024-10-04 09:17:17 +0700

    update

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -21,7 +21,7 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body className={inter.className}>
-        <ClerkProvider>
+        <ClerkProvider publishableKey={process.env.NEXT_PUBLIC_CLERK_PUBLISHABLE_KEY}>
           <NavBarApp />
           <MenuOffCanvas />
         </ClerkProvider>
