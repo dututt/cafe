4185917fd2a86670e62e45b1d8745f4d3e7982e2
dutu <du.tu@hitachivantara.com>
2024-10-10 16:05:51 +0700

    update

--- src/app/admin/menu.offcanvas.tsx
+++ src/app/admin/menu.offcanvas.tsx
@@ -1,85 +1,101 @@
-'use client'
-import { Offcanvas } from 'react-bootstrap';
-import Container from 'react-bootstrap/Container';
-import Form from 'react-bootstrap/Form';
-import Nav from 'react-bootstrap/Nav';
-import Navbar from 'react-bootstrap/Navbar';
-import NavDropdown from 'react-bootstrap/NavDropdown';
-import { useClerk } from '@clerk/nextjs';
-import { useEffect, useState } from 'react';
-import { viewOrder } from './actions';
+"use client";
+import { Offcanvas } from "react-bootstrap";
+import Container from "react-bootstrap/Container";
+import Form from "react-bootstrap/Form";
+import Nav from "react-bootstrap/Nav";
+import Navbar from "react-bootstrap/Navbar";
+import NavDropdown from "react-bootstrap/NavDropdown";
+import { useClerk } from "@clerk/nextjs";
+import { useEffect, useState } from "react";
+import { viewOrder } from "./actions";
 
 function MenuOffCanvas() {
-    const { user, signOut } = useClerk();
-    const [role, setRole] = useState<boolean>(false)
+  const { user, signOut } = useClerk();
+  const [role, setRole] = useState<boolean>(false);
 
-    useEffect(() => {
-        getRole()
-    })
+  useEffect(() => {
+    getRole();
+  }, [user]);
 
-    const getRole = () => {
-        viewOrder().then(res => {
-            setRole(res)
-        })
-        return true
-    }
+  const getRole = () => {
+    viewOrder().then((res) => {
+      setRole(res);
+    });
+    return true;
+  };
 
-    return (
-        <>
-            {
-                ['sm'].map((expand) => (
-                    <Navbar key={expand} expand={expand} className="bg-body-tertiary">
-                        <Container >
-                            <Navbar.Toggle aria-controls={`offcanvasNavbar-expand-${expand}`} />
-                            <Form className="d-flex">
-                                <Form.Control
-                                    type="search"
-                                    placeholder="Tìm kiếm"
-                                    className="me-2"
-                                    aria-label="Tìm kiếm"
-                                />
-                            </Form>
+  return (
+    <>
+      {["sm"].map((expand) => (
+        <Navbar key={expand} expand={expand} className="bg-body-tertiary">
+          <Container>
+            <Navbar.Toggle aria-controls={`offcanvasNavbar-expand-${expand}`} />
+            <Form className="d-flex">
+              <Form.Control
+                type="search"
+                placeholder="Tìm kiếm"
+                className="me-2"
+                aria-label="Tìm kiếm"
+              />
+            </Form>
 
-                            {user?.primaryEmailAddress?.emailAddress && <Navbar.Offcanvas
-                                id={`offcanvasNavbar-expand-${expand}`}
-                                aria-labelledby={`offcanvasNavbarLabel-expand-${expand}`}
-                                placement="start"
-                            >
-                                <Offcanvas.Header closeButton>
-                                    <Offcanvas.Title id={`offcanvasNavbarLabel-expand-${expand}`}>
-                                        Quản lý
-                                    </Offcanvas.Title>
-                                </Offcanvas.Header>
-                                <Offcanvas.Body>
-                                    <Nav className="justify-content-end flex-grow-1 pe-3">
-                                        <Nav.Link href="/">Home</Nav.Link>
-                                        <Nav.Link href="/orders-status">Trạng thái</Nav.Link>
-                                        <Nav.Link hidden={role} href="/orders-management">Đơn hàng</Nav.Link>
-                                        <Nav.Link hidden={role} href="/food-beverage-management">Thêm món mới</Nav.Link>
-                                        <Nav.Link hidden={role} href="/admin">Quản lý</Nav.Link>
-                                        <NavDropdown hidden={role}
-                                            title="Doanh thu"
-                                            id={`offcanvasNavbarDropdown-expand-${expand}`}
-                                        >
-                                            <NavDropdown.Item href="/report">Theo ngày</NavDropdown.Item>
-                                            <NavDropdown.Item href="#action4">
-                                                Theo tháng
-                                            </NavDropdown.Item>
-                                            <NavDropdown.Divider />
-                                            <NavDropdown.Item href="#action5">
-                                                Theo năm
-                                            </NavDropdown.Item>
-                                        </NavDropdown>
-                                        <Nav.Link onClick={async () => { await signOut() }} href="/">Đăng xuất</Nav.Link>
-                                    </Nav>
-                                </Offcanvas.Body>
-                            </Navbar.Offcanvas>}
-                        </Container>
-                    </Navbar>
-                ))
-            }
-        </>
-    );
+            {user?.primaryEmailAddress?.emailAddress && (
+              <Navbar.Offcanvas
+                id={`offcanvasNavbar-expand-${expand}`}
+                aria-labelledby={`offcanvasNavbarLabel-expand-${expand}`}
+                placement="start"
+              >
+                <Offcanvas.Header closeButton>
+                  <Offcanvas.Title id={`offcanvasNavbarLabel-expand-${expand}`}>
+                    Quản lý
+                  </Offcanvas.Title>
+                </Offcanvas.Header>
+                <Offcanvas.Body>
+                  <Nav className="justify-content-end flex-grow-1 pe-3">
+                    <Nav.Link href="/">Home</Nav.Link>
+                    <Nav.Link href="/orders-status">Trạng thái</Nav.Link>
+                    <Nav.Link hidden={role} href="/orders-management">
+                      Đơn hàng
+                    </Nav.Link>
+                    <Nav.Link hidden={role} href="/food-beverage-management">
+                      Thêm món mới
+                    </Nav.Link>
+                    <Nav.Link hidden={role} href="/admin">
+                      Quản lý
+                    </Nav.Link>
+                    <NavDropdown
+                      hidden={role}
+                      title="Doanh thu"
+                      id={`offcanvasNavbarDropdown-expand-${expand}`}
+                    >
+                      <NavDropdown.Item href="/report">
+                        Theo ngày
+                      </NavDropdown.Item>
+                      <NavDropdown.Item href="#action4">
+                        Theo tháng
+                      </NavDropdown.Item>
+                      <NavDropdown.Divider />
+                      <NavDropdown.Item href="#action5">
+                        Theo năm
+                      </NavDropdown.Item>
+                    </NavDropdown>
+                    <Nav.Link
+                      onClick={async () => {
+                        await signOut();
+                      }}
+                      href="/"
+                    >
+                      Đăng xuất
+                    </Nav.Link>
+                  </Nav>
+                </Offcanvas.Body>
+              </Navbar.Offcanvas>
+            )}
+          </Container>
+        </Navbar>
+      ))}
+    </>
+  );
 }
 
 export default MenuOffCanvas;
--- src/app/admin/navbar.tsx
+++ src/app/admin/navbar.tsx
@@ -1,63 +1,62 @@
-'use client'
-import Container from 'react-bootstrap/Container';
-import Navbar from 'react-bootstrap/Navbar';
-import { useState } from 'react';
-import { Modal, Placeholder, ProgressBar } from 'react-bootstrap';
-import Register from '../../components/register';
-import { useClerk } from '@clerk/nextjs';
-import Link from 'next/link';
+"use client";
+import Container from "react-bootstrap/Container";
+import Navbar from "react-bootstrap/Navbar";
+import { useState } from "react";
+import { Modal, Placeholder, ProgressBar } from "react-bootstrap";
+import Register from "../../components/register";
+import { useClerk } from "@clerk/nextjs";
+import Link from "next/link";
 
 function NavBarApp() {
+  const { user } = useClerk();
 
-    const { user } = useClerk();
+  const [showLogin, setShowLogin] = useState(false);
+  const [showRegister, setShowRegister] = useState(false);
+  const handleClose = () => setShowLogin(false);
 
-    const [showLogin, setShowLogin] = useState(false);
-    const [showRegister, setShowRegister] = useState(false);
-    const handleClose = () => setShowLogin(false);
+  const handleCloseRegister = () => setShowRegister(false);
+  const handleShowRegister = () => {
+    setShowLogin(false);
+    setShowRegister(true);
+  };
 
-    const handleCloseRegister = () => setShowRegister(false);
-    const handleShowRegister = () => {
-        setShowLogin(false);
-        setShowRegister(true);
-    }
+  return (
+    <div>
+      <Navbar className="bg-body-tertiary">
+        <Container>
+          <Navbar.Brand href="/">Cafe 290</Navbar.Brand>
 
-    return (
-        <div>
-            <Navbar className="bg-body-tertiary">
-                <Container>
-                    <Navbar.Brand href="/">Cafe 290</Navbar.Brand>
+          <Navbar.Toggle />
+          <Navbar.Collapse className="justify-content-end">
+            {user?.primaryEmailAddress?.emailAddress ?? (
+              <Link href="/login">Login</Link>
+            )}
+          </Navbar.Collapse>
+        </Container>
+      </Navbar>
 
-                    <Navbar.Toggle />
-                    <Navbar.Collapse className="justify-content-end">
-                        {user?.primaryEmailAddress?.emailAddress ?? <Link href='/login'>
-                            Login
-                        </Link>}
-                    </Navbar.Collapse>
-                </Container>
-            </Navbar>
+      <Modal show={showLogin} onHide={handleClose}>
+        <Modal.Header closeButton>
+          <Modal.Title>Đăng Nhập</Modal.Title>
+        </Modal.Header>
+        <Modal.Body>
+          {/* <LoginCard refreshChangeText={refreshChangeText} /> */}
+          <Navbar.Brand onClick={handleShowRegister} href="#">
+            Đăng Ký
+          </Navbar.Brand>
+        </Modal.Body>
+      </Modal>
 
-
-            <Modal show={showLogin} onHide={handleClose}>
-                <Modal.Header closeButton>
-                    <Modal.Title>Đăng Nhập</Modal.Title>
-                </Modal.Header>
-                <Modal.Body>
-                    {/* <LoginCard refreshChangeText={refreshChangeText} /> */}
-                    <Navbar.Brand onClick={handleShowRegister} href="#">Đăng Ký</Navbar.Brand>
-                </Modal.Body>
-            </Modal>
-
-
-            <Modal show={showRegister} onHide={handleCloseRegister}>
-                <Modal.Header closeButton>
-                    <Modal.Title>Đăng Ký</Modal.Title>
-                </Modal.Header>
-                <Modal.Body>
-                    <Register />
-                </Modal.Body>
-            </Modal>
-        </div>
-    );
+      <Modal show={showRegister} onHide={handleCloseRegister}>
+        <Modal.Header closeButton>
+          <Modal.Title>Đăng Ký</Modal.Title>
+        </Modal.Header>
+        <Modal.Body>
+          <Register />
+        </Modal.Body>
+      </Modal>
+    </div>
+  );
 }
 
 export default NavBarApp;
